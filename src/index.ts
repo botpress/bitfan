1c82c2c260aba9b01cc0291deb362f9b10c0ae50
Francois <francois_levasseur@hotmail.com>
2020-10-07 12:58:09 -0400

    extract runSolution in multiple functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import * as sdk from "src/bitfan";
-import chalk from "chalk";
 
 import {
   mostConfidentBinaryScore,
@@ -19,79 +18,10 @@ import { trainTestSplit } from "./builtin/tools/trainTestSplit";
 import { splitOOS, pickOOS } from "./builtin/tools/splitAndMakeOOS";
 
 import { BpIntentEngine } from "./builtin/engines/intent";
-import { sleep } from "./utils";
-
 import { areSame, isOOS, makeKey } from "./services/labels";
+import runSolution from "./solution";
 
 const dsRepo = new DatasetRepository();
-
-const runSolution = async <T extends sdk.ProblemType>(
-  solution: sdk.Solution<T>,
-  seeds: number[]
-) => {
-  const { engine, metrics, name, problems } = solution;
-
-  const allResults: sdk.Result<T>[] = [];
-
-  for (const seed of seeds) {
-    console.log(
-      chalk.green(chalk.bold(`Running Solution ${name} with seed ${seed}`))
-    );
-
-    const solutionResults: sdk.Result<T>[] = [];
-
-    for (const problem of problems) {
-      try {
-        console.log(chalk.green(chalk.bold(`Problem ${problem.name}`)));
-
-        await engine.train(problem.trainSet, seed);
-
-        await sleep(1000);
-
-        const predictOutputs = await engine.predict(problem.testSet);
-        const results: sdk.Result<T>[] = predictOutputs.map((p) => {
-          const metricsNames = metrics.map((m) => m.name);
-          const metricsScores = metrics.map((m) => m.eval(p));
-          return {
-            ...p,
-            scores: _.zipObject(metricsNames, metricsScores),
-            metadata: {
-              seed,
-              problem: problem.name,
-            },
-          };
-        });
-
-        solutionResults.push(...results);
-
-        if (!!problem.cb) {
-          console.log(
-            chalk.green(chalk.bold(`Results for Problem ${problem.name}`))
-          );
-          await problem.cb(results);
-          console.log("\n");
-        }
-      } catch (err) {
-        console.log(
-          chalk.red(
-            `The following error occured when solving problem ${problem.name} with seed ${seed}:\n${err.message}`
-          )
-        );
-        process.exit(1);
-      }
-    }
-
-    allResults.push(...solutionResults);
-
-    if (!!solution.cb) {
-      console.log(chalk.green(chalk.bold(`Results for All Problems`)));
-      await solution.cb(solutionResults);
-      console.log("\n");
-    }
-  }
-
-  return allResults;
-};
 
 // TODO: write actual implementation
 const impl: typeof sdk = {
---
+++ src/solution.ts
@@ -0,0 +1,99 @@
+import * as sdk from "bitfan/sdk";
+import chalk from "chalk";
+import _ from "lodash";
+import { sleep } from "./utils";
+
+const runSolution = async <T extends sdk.ProblemType>(
+  solution: sdk.Solution<T>,
+  seeds: number[]
+) => {
+  const allResults: sdk.Result<T>[] = [];
+
+  const runSolutionForSeed = makeSolutionRunner({ allResults }, solution);
+  for (const seed of seeds) {
+    await runSolutionForSeed(seed);
+  }
+
+  return allResults;
+};
+
+const makeSolutionRunner = <T extends sdk.ProblemType>(
+  state: {
+    allResults: sdk.Result<T>[];
+  },
+  solution: sdk.Solution<T>
+) => async (seed: number) => {
+  const { allResults } = state;
+
+  const { name, problems } = solution;
+
+  console.log(
+    chalk.green(chalk.bold(`Running Solution ${name} with seed ${seed}`))
+  );
+
+  const solutionResults: sdk.Result<T>[] = [];
+
+  const runProblem = makeProblemRunner({ solutionResults }, solution, seed);
+  for (const problem of problems) {
+    try {
+      await runProblem(problem);
+    } catch (err) {
+      console.log(
+        chalk.red(
+          `The following error occured when solving problem ${problem.name} with seed ${seed}:\n${err.message}`
+        )
+      );
+      process.exit(1);
+    }
+  }
+
+  allResults.push(...solutionResults);
+
+  if (!!solution.cb) {
+    console.log(chalk.green(chalk.bold(`Results for All Problems`)));
+    await solution.cb(solutionResults);
+    console.log("\n");
+  }
+};
+
+const makeProblemRunner = <T extends sdk.ProblemType>(
+  state: {
+    solutionResults: sdk.Result<T>[];
+  },
+  solution: sdk.Solution<T>,
+  seed: number
+) => async (problem: sdk.Problem<T>) => {
+  const { solutionResults } = state;
+
+  const { engine, metrics } = solution;
+
+  console.log(chalk.green(chalk.bold(`Problem ${problem.name}`)));
+
+  await engine.train(problem.trainSet, seed);
+
+  await sleep(500);
+
+  const predictOutputs = await engine.predict(problem.testSet);
+  const results: sdk.Result<T>[] = predictOutputs.map((p) => {
+    const metricsNames = metrics.map((m) => m.name);
+    const metricsScores = metrics.map((m) => m.eval(p));
+    return {
+      ...p,
+      scores: _.zipObject(metricsNames, metricsScores),
+      metadata: {
+        seed,
+        problem: problem.name,
+      },
+    };
+  });
+
+  solutionResults.push(...results);
+
+  if (!!problem.cb) {
+    console.log(chalk.green(chalk.bold(`Results for Problem ${problem.name}`)));
+    await problem.cb(results);
+    console.log("\n");
+  }
+};
+
+export default runSolution;
