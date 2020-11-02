46cdda405f2a84b7fc8b1413e14e3ad3c07914b2
Francois <francois_levasseur@hotmail.com>
2020-11-02 14:29:53 -0500

    better visualise oos and avgscore

--- src/builtin/visualisation/avgScores.ts
+++ src/builtin/visualisation/avgScores.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import _ from "lodash";
 import * as sdk from "src/bitfan";
+import { roundNumbers2Level } from "../../services/logging";
 import { averageScores } from "../metrics/avgScores";
 
 export const showAverageScores: typeof sdk.visualisation.showAverageScores = async <
@@ -11,5 +12,5 @@ export const showAverageScores: typeof sdk.visualisation.showAverageScores = asy
 ) => {
   const scores = await averageScores(results, options);
   console.log(chalk.green(`Average Score By Metrics`));
-  console.table(scores);
+  console.table(roundNumbers2Level(scores, 4));
 };
--- src/builtin/visualisation/oos.ts
+++ src/builtin/visualisation/oos.ts
@@ -1,33 +1,48 @@
 import * as sdk from "bitfan/sdk";
 import chalk from "chalk";
 import _ from "lodash";
+import { roundNumbers2Level } from "../../services/logging";
 import { oosConfusion, oosPerformance } from "../metrics/oos";
 
 export const showOOSConfusion: typeof sdk.visualisation.showOOSConfusion = async (
   results: sdk.Result<sdk.SingleLabel>[]
 ) => {
-  const { truePos, falsePos, falseNeg, trueNeg } = await oosConfusion(results);
+  const res = await oosConfusion(results, { groupBy: "all" });
+  const { truePos, falsePos, falseNeg, trueNeg } = res["all"];
 
   const confusionMatrix = {
-    electedIsOOScope: {
-      actualIsOOScope: truePos,
-      actualIsInScope: falsePos,
+    "elected is oo-scope": {
+      "actual is oo-scope": truePos,
+      "actual is in-scope": falsePos,
     },
-    electedIsInScope: {
-      actualIsOOScope: falseNeg,
-      actualIsInScope: trueNeg,
+    "elected is in-scope": {
+      "actual is oo-scope": falseNeg,
+      "actual is in-scope": trueNeg,
     },
   };
 
   console.log(chalk.green("OOS Confusion Matrix: "));
-  console.table(confusionMatrix);
+  console.table(roundNumbers2Level(confusionMatrix, 4));
 };
 
 export const showOOSPerformance: typeof sdk.visualisation.showOOSPerformance = async (
   results: sdk.Result<sdk.SingleLabel>[],
   options?: Partial<sdk.AggregateOptions>
 ) => {
-  const performance = await oosPerformance(results, options);
+  const table = await oosPerformance(results, options);
+
+  const performance = _.mapValues(
+    table,
+    ({ accuracy, precision, recall, f1 }) => {
+      return {
+        "oos accuracy": accuracy,
+        "oos precision": precision,
+        "oos recall": recall,
+        "oos f1": f1,
+      };
+    }
+  );
+
   console.log(chalk.green("OOS Performance metrics: "));
-  console.table(performance);
+  console.table(roundNumbers2Level(performance));
 };
